d4ceaa8b56423181dd97cde6685bc8698ea43ef7
Muskan Sahu <muskansahu3306@gmail.com>
2026-06-26 15:24:41 +0530

    Day 4: HashMap

---
+++ src/com/muskan/HashMapDemo.java
@@ -0,0 +1,17 @@
+package com.muskan;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> map = new HashMap<Integer, String>();
+		map.put(1,"Muskan");
+		map.put(2,"Mehek");
+		map.put(3,"Muskan");
+		map.put(4,"Palak");
+		map.put(5,"Sia");
+		System.out.println(map);
+	}
+
+}
