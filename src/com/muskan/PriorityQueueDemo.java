2d76e6db81b5cc2bd755cdc3411bc2ec3eace760
Muskan Sahu <muskansahu3306@gmail.com>
2026-06-26 15:20:31 +0530

    Day 4: Priority queue

---
+++ src/com/muskan/PriorityQueueDemo.java
@@ -0,0 +1,16 @@
+package com.muskan;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(30);
+		pq.add(10);
+		pq.add(40);
+		pq.add(20);
+		System.out.println(pq);
+	}
+
+}
